c13edacaec570e6a28f74225d90a756a35b87190
HectorCdn <hcottachaudon@edenschool.fr>
2024-11-05 17:12:42 +0100

    feat yyyazf

---
+++ index.js
@@ -0,0 +1,53 @@
+// Exercice 1
+
+function addition(a, b) {
+	return a + b;
+}
+console.log(addition(2, 3));
+
+// Exercice 2
+
+function salutation(nom) {
+	return "Bonjour " + nom;
+}
+console.log(salutation("Alice"));
+
+// Exercice 3
+
+function estPair(nombre) {
+	return nombre % 2 === 0;
+}
+console.log(estPair(4));
+console.log(estPair(7));
+
+// Exercice 4
+
+function aireRectangle(longueur, largeur) {
+	return longueur * largeur;
+}
+console.log(aireRectangle(5, 3));
+
+// Exercice 5
+
+function estMajuscule(chaine) {
+	return chaine === chaine.toUpperCase();
+}
+console.log(estMajuscule("HELLO"));
+console.log(estMajuscule("Hello"));
+
+// Exercice 6
+
+function maxDeuxNombres(a, b) {
+	return a, b;
+}
+console.log(maxDeuxNombres(5, 10)); // Résultat attendu : 10
+
+// Exercice 7
+
+// Exercice 8
+
+// Exercice 9
+
+// Exercice 10
+
+// Exercice 11
