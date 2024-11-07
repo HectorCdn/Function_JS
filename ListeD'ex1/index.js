26081c85a4207c2fd59b8fb52857a19af50c3136
HectorCdn <hcottachaudon@edenschool.fr>
2024-11-07 17:13:22 +0100

    sgijio

--- ListeD'ex1/index.js
+++ ListeD'ex1/index.js
@@ -44,27 +44,72 @@ console.log(maxDeuxNombres(5, 10));
 
 // Exercice 7
 
-
+function difference(a, b) {
+  return (a - b)
+}
+console.log(difference(10, 3));
 
 // Exercice 8
 
-
+function convertirEnCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5/9
+}
+console.log(convertirEnCelsius(100));
 
 // Exercice 9
 
-
+function estMajeur(age) {
+  return (age >= 18)
+}
+console.log(estMajeur(20));
+console.log(estMajeur(16));
 
 // Exercice 10
 
+function maxTroisNombres(a, b, c) {
+  return Math.max(a, b, c);
+}
+
+console.log(maxTroisNombres(3, 7, 5));
 
 
 // Exercice 11
 
+function calculer(a, b, operation) {
+  return operation(a, b);
+}
+
+function addition(x, y) {
+  return x + y;
+}
+
+function multiplication(x, y) {
+  return x * y;
+}
+
+console.log(calculer(5, 3, addition));
+console.log(calculer(5, 3, multiplication));
 
 
 // Exercice 12
 
+function estVoyelle(c) {
 
+  return "aeiouAEIOU".includes(c);
+}
+
+function filtrerTexte(chaine, filtre) {
+
+  let resultat = "";
+  for (let i = 0; i < chaine.length; i++) {
+    if (filtre(chaine[i])) {
+      resultat += chaine[i];
+    }
+  }
+  return resultat;
+}
+
+console.log(filtrerTexte("Bonjour", estVoyelle));
 
 // Exercice 13
 
