af348ce50e6b6e14993ccefe327994aca4e8b377
HectorCdn <hcottachaudon@edenschool.fr>
2024-11-08 13:40:44 +0100

    feat

---
+++ ListeD'ex1/ztap.js
@@ -0,0 +1,44 @@
+let prenom = ""
+let nom = ""
+let age = Number(0)
+let adresse 
+
+if (adresse.include("@")) {
+    console.log("Adresse invalide.")
+    break;
+} else () {
+    alert("Adresse invalide")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
